b23737be5d7c6baf0ed99d8359da093a93c87fb1
entervuapp <entervuapp@gmail.com>
2020-06-08 19:14:58 +0530

    autoComplete changes

--- src/app/commons/components/carousel/carousel.component.ts
+++ src/app/commons/components/carousel/carousel.component.ts
@@ -34,18 +34,18 @@ export class CarouselComponent implements OnInit {
   }
 
   slickInit(e) {
-    console.log("slick initialized");
+    // console.log("slick initialized");
   }
 
   breakpoint(e) {
-    console.log("breakpoint");
+    // console.log("breakpoint");
   }
 
   afterChange(e) {
-    console.log("afterChange");
+    // console.log("afterChange");
   }
 
   beforeChange(e) {
-    console.log("beforeChange");
+    // console.log("beforeChange");
   }
 }
