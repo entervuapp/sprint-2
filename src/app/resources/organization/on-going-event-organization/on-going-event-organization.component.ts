4e0973008a3f3d6e6814e253623f6e28ee5d1493
Awadhesh Roy <rawadhesh@DESKTOP-CG6C9LG.pkrmsezdc.com>
2020-06-17 09:59:19 +0530

    dynamic carousel made

--- src/app/resources/organization/on-going-event-organization/on-going-event-organization.component.ts
+++ src/app/resources/organization/on-going-event-organization/on-going-event-organization.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from "@angular/core";
 import { Subscription } from "rxjs";
 import { ActivatedRoute } from "@angular/router";
 import { Alerts, SkillWithCount } from "../../../commons/typings/typings";
+import { ManageEventsService } from "../../../resources/organization/manage-events/manage-events/manage-events.service";
+import { ManageCandidateService } from "../../../resources/organization/manage-candidates/manage-candidates/manage-candidate.service";
 
 @Component({
   selector: "app-on-going-event-organization",
@@ -13,12 +15,22 @@ export class OnGoingEventOrganizationComponent implements OnInit {
   public eventId: number;
   public alerts: Alerts[];
   public selectedSlide: SkillWithCount;
-  constructor(private activatedRoute: ActivatedRoute) {}
+  public eventDetails: object;
+  public candidatesList: any[];
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private manageEventsService: ManageEventsService,
+    private manageCandidateService: ManageCandidateService
+  ) {}
 
   ngOnInit() {
     this._subscriptions.add(
       this.activatedRoute.queryParams.subscribe((params) => {
         this.eventId = parseInt(params["id"]);
+        if (this.eventId) {
+          this.getEventDetails(this.eventId);
+        }
       })
     );
   }
@@ -29,5 +41,43 @@ export class OnGoingEventOrganizationComponent implements OnInit {
 
   public onSelectOfCarousel = (slide: SkillWithCount): void => {
     this.selectedSlide = { ...slide };
+  };
+
+  private getEventDetails = (eventId) => {
+    if (eventId) {
+      this._subscriptions.add(
+        this.manageEventsService.findEvent(eventId).subscribe(
+          (response) => {
+            this.eventDetails = { ...response };
+            this.getCandidatesList(this.eventId);
+          },
+          (errors) => {
+            console.log("error", errors);
+            if (errors) {
+              this.alerts = [{ code: "ERROR", systemMessage: errors }];
+            }
+          }
+        )
+      );
+    }
+  };
+
+  private getCandidatesList = (eventId): void => {
+    this._subscriptions.add(
+      this.manageCandidateService.getCandidates().subscribe(
+        (response) => {
+          let allCandidatesList = [...response];
+          this.candidatesList = allCandidatesList.filter(
+            (item) => item.eventId === eventId
+          );
+        },
+        (errors) => {
+          console.log("error", errors);
+          if (errors) {
+            this.alerts = [{ code: "ERROR", systemMessage: errors }];
+          }
+        }
+      )
+    );
   };
 }
